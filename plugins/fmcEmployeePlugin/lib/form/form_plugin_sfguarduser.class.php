7472245ae65b605a097d9b66dc4f9595d4e820c7
Yasin Aydin <yasin@yasinaydin.net>
2012-03-21 15:54:20 +0200

    [Employees] now showing permissions list in order

--- plugins/fmcEmployeePlugin/lib/form/form_plugin_sfguarduser.class.php
+++ plugins/fmcEmployeePlugin/lib/form/form_plugin_sfguarduser.class.php
@@ -27,6 +27,12 @@ class form_plugin_sfguarduser extends sfGuardUserForm
       array('invalid' => 'The two passwords must be the same.'))
     );
     
+    $this->setWidget('permissions_list', new sfWidgetFormDoctrineChoice(array(
+      'model' => $this->getRelatedModelName('Permissions'),
+      'table_method' => 'getOrdered',
+      'add_empty' => false)
+    ));
+    
     $this->widgetSchema['permissions_list'] = new sfWidgetFormSelectDoubleList(array(
       'choices' => $this->widgetSchema['permissions_list']->getChoices(), 
       'label_associated' => 'Given Permissions',
--- plugins/sfDoctrineGuardPlugin/lib/model/doctrine/PluginsfGuardPermissionTable.class.php
+++ plugins/sfDoctrineGuardPlugin/lib/model/doctrine/PluginsfGuardPermissionTable.class.php
@@ -1,13 +1,11 @@
 <?php
 
-/**
- * Permission table.
- *
- * @package    sfDoctrineGuardPlugin
- * @subpackage model
- * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
- * @version    SVN: $Id: PluginsfGuardPermissionTable.class.php 23793 2009-11-11 17:42:50Z Kris.Wallsmith $
- */
 abstract class PluginsfGuardPermissionTable extends Doctrine_Table
 {
+  public function getOrdered()
+  {
+    return $this->CreateQuery ('perm')
+      ->orderBy ('perm.name ASC')
+      ->execute();
+  }
 }
