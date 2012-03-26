d402d97e6d93f66e50bbca1a57bd930900304935
Yasin Aydin <yasin@yasinaydin.net>
2012-03-26 22:40:19 +0300

    [WHDB] enter and exit checks added

--- plugins/fmcWorkingHourPlugin/lib/FmcWhCheck.class.php
+++ plugins/fmcWorkingHourPlugin/lib/FmcWhCheck.class.php
@@ -8,15 +8,63 @@ class FmcWhCheck
     $this->user = sfContext::getInstance()->getUser();
   }
   
+  public function getLastEnter ($date)
+  {
+    $query = Doctrine::getTable('WorkingHour')
+      ->createQuery ('wh')
+      ->addWhere ('wh.user_id = ?', $this->user->getGuardUser()->getId())
+      ->addWhere ('wh.date = ?', $date)
+      ->addWhere ('wh.type = ?', 'Enter')
+      ->addOrderBy ('wh.start DESC')
+      ->limit(1);
+    return $query->fetchOne();
+  }
+  
+  public function getLastExit ($date)
+  {
+    $query = Doctrine::getTable('WorkingHour')
+      ->createQuery ('wh')
+      ->addWhere ('wh.user_id = ?', $this->user->getGuardUser()->getId())
+      ->addWhere ('wh.date = ?', $date)
+      ->addWhere ('wh.type = ?', 'Exit')
+      ->addOrderBy ('wh.end DESC')
+      ->limit(1);
+    return $query->fetchOne();
+  }
+  
   public function CanEnter ($date)
   {
     $status = "";
+    $sen = $this->getLastEnter($date);
+    $sex = $this->getLastExit($date);
+    
+    // eger giris de cikis da yoksa izin ver
+    if ($sen or $sex) //eger giris veya cikistan en az biri varsa
+    {
+      if ($sen and !$sex) //sadece giris varsa
+        $status = "You have unexited jobs.";
+      elseif ($sen and $sex)
+        if ($sen > $sex)
+          $status = "First you have to exit your last work";
+    }
+    
     return $status;
   }
   
   public function CanExit ($date)
   {
     $status = "";
+    $sen = $this->getLastEnter($date);
+    $sex = $this->getLastExit($date);
+    
+    if (!$sen and !$sex) //giris de cikis da yoksa
+      $status = "First you have to enter.";
+    elseif ($sex and !$sen) //giris yok cikisi varsa
+      $status = "First you have to enter.";
+    elseif ($sen and $sex)
+      if ($sex > $sen) //cikis giristen sonraysa
+        $status = "First you have to enter the office";
+    
     return $status;
   }
   
