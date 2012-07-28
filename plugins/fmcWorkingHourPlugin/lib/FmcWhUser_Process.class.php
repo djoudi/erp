28d14888356ad8a2c24881d42524b86df7571acd
Yasin Aydin <yasin@yasinaydin.net>
2012-07-28 20:34:40 +0300

    Fixed leave request class removal

--- plugins/fmcWorkingHourPlugin/lib/FmcWhUser_Process.class.php
+++ plugins/fmcWorkingHourPlugin/lib/FmcWhUser_Process.class.php
@@ -12,6 +12,32 @@ class FmcWhUser_Process {
     
     /*################################################################################*/
     
+    public function workingHour_DayLeaveRequest ($form, $request, $redirectUrl) {
+        
+        if ($request->isMethod('post')) {
+          
+            $form->bind ($request->getParameter($form->getName()));
+            
+            if ($form->isValid()) {
+
+                $object = $form->save();
+                $object->setUpdatedBy($this->user->getGuardUser()->getId());
+                $object->setCreatedBy($this->user->getGuardUser()->getId());
+                $object->save();
+                
+                $this->user->setFlash('success', 'Leave request has been sent.');
+                $this->controller->redirect ($redirectUrl);
+                
+            } else {
+                $this->user->setFlash('error', 'Problem occured saving the record! Please check your input.');
+        
+            }
+        }
+    }
+    
+    /*################################################################################*/
+    
+    
     public function workingHour_MonthyHourSetAll ($request, $redirectUrl) {
     
         if ($request->isMethod('post')) {
--- plugins/fmcWorkingHourPlugin/modules/wh_Reports/templates/_dailyreport.php
+++ plugins/fmcWorkingHourPlugin/modules/wh_Reports/templates/_dailyreport.php
@@ -7,53 +7,59 @@
 
 <table class="table table-condensed table-bordered">
     
-    <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Department</th>
-        <th>Entrance</th>
-        <th>Exit</th>
-    </tr>
-    
-    <?php $i=1; ?>
-    
-    <?php foreach ($users as $user): ?>
-        <?php $status = $user->getDayStatusFor($date); ?>
+    <thead>
         <tr>
-            <td>
-                <?php echo $i; ?>
-            </td>
-            <td>
-                <?php echo $user["first_name"]." ".$user["last_name"];#->__toString(); ?>
-            </td>
-            <td>
-                <?php echo $user["Department"]["name"]; ?>
-            </td>
-            
-            <?php if ($status=="leave"): ?>
-            
-                <td colspan="2">
-                    leave
-                </td>
-                
-            <?php elseif ($status=="work"): ?>
-            
-                <td>
-                    <?php echo $user->getEntranceFor ($date); ?>
-                </td>
-                <td>
-                    <?php echo $user->getExitFor ($date); ?>
-                </td>
-                
-            <?php elseif ($status=="empty"): ?>
-            
-                <td colspan="2">-</td>
-           
-            <?php endif; ?>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Entrance</th>
+            <th>Exit</th>
         </tr>
+    </thead>
+    
+    <tbody>
+    
+        <?php $i=1; ?>
         
-        <?php $i++; ?>
-        
-    <?php endforeach; ?>
+        <?php foreach ($users as $user): ?>
+            <?php $status = $user->getDayStatusFor($date); ?>
+            <tr>
+                <td>
+                    <?php echo $i; ?>
+                </td>
+                <td>
+                    <?php echo $user["first_name"]." ".$user["last_name"];#->__toString(); ?>
+                </td>
+                <td>
+                    <?php echo $user["Department"]["name"]; ?>
+                </td>
+                
+                <?php if ($status=="leave"): ?>
+                
+                    <td colspan="2">
+                        leave
+                    </td>
+                    
+                <?php elseif ($status=="work"): ?>
+                
+                    <td>
+                        <?php echo $user->getEntranceFor ($date); ?>
+                    </td>
+                    <td>
+                        <?php echo $user->getExitFor ($date); ?>
+                    </td>
+                    
+                <?php elseif ($status=="empty"): ?>
+                
+                    <td colspan="2">-</td>
+               
+                <?php endif; ?>
+            </tr>
+            
+            <?php $i++; ?>
+            
+        <?php endforeach; ?>
+    
+    </tbody>
     
 </table>
