83dca4e54c057210ac0878b9c11fbeba21ccf13d
Yasin Aydin <yasin@yasinaydin.net>
2012-09-03 23:32:41 +0300

    #51 - new ve edit user lar guncellendi - OK

--- plugins/fmcEmployeePlugin/lib/form/form_plugin_sfguarduser.class.php
+++ plugins/fmcEmployeePlugin/lib/form/form_plugin_sfguarduser.class.php
@@ -27,6 +27,19 @@ class form_plugin_sfguarduser extends sfGuardUserForm
       array('invalid' => 'The two passwords must be the same.'))
     );
     
+    
+    
+    /* Refs #51 */
+    $this->setWidget('group_id', new sfWidgetFormDoctrineChoice(array(
+        #'multiple' => false,
+        'model' => 'sfGuardGroup',
+        'add_empty' => false
+    )));
+    $this->validatorSchema['group_id']->setOption('required', true);
+    /* End of Refs #51 */
+    
+    
+    
     $this->setWidget('permissions_list', new sfWidgetFormDoctrineChoice(array(
       'model' => $this->getRelatedModelName('Permissions'),
       'table_method' => 'getOrdered',
