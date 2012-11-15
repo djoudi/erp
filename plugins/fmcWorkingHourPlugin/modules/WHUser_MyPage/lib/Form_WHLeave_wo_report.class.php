92b051abd74b01d6f4b862488662f94291bc5af1
Yasin Aydin <yasin@yasinaydin.net>
2012-11-15 03:49:07 +0200

    [Fixed] renamed report comment to number on forms

--- plugins/fmcWorkingHourPlugin/modules/WHUser_MyPage/lib/Form_WHLeave_wo_report.class.php
+++ plugins/fmcWorkingHourPlugin/modules/WHUser_MyPage/lib/Form_WHLeave_wo_report.class.php
@@ -6,7 +6,7 @@ class Form_WHLeave_wo_report extends Form_WHLeave_w_report
     {
         parent::configure();
 
-        unset($this['report_Comment']);
+        unset($this['report_Number']);
         unset($this['report_Date']);
         unset($this['report_Received']);
     }  	
