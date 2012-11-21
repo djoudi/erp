a56a2f479db0bebea3b7389a8ca43f64d17304d2
Yasin Aydin <yasin@yasinaydin.net>
2012-11-21 10:03:59 +0200

    Entrance and exit is now in day model

--- lib/filter/doctrine/fmcWorkingHourPlugin/base/BaseWorkingHourDayVersionFormFilter.class.php
+++ lib/filter/doctrine/fmcWorkingHourPlugin/base/BaseWorkingHourDayVersionFormFilter.class.php
@@ -13,29 +13,33 @@ abstract class BaseWorkingHourDayVersionFormFilter extends BaseFormFilterDoctrin
   public function setup()
   {
     $this->setWidgets(array(
-      'user_id'    => new sfWidgetFormFilterInput(array('with_empty' => false)),
-      'leave_id'   => new sfWidgetFormFilterInput(),
-      'date'       => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
-      'status'     => new sfWidgetFormChoice(array('choices' => array('' => '', 'Draft' => 'Draft', 'Pending' => 'Pending', 'Accepted' => 'Accepted', 'Denied' => 'Denied'))),
-      'multiplier' => new sfWidgetFormFilterInput(array('with_empty' => false)),
-      'creater_id' => new sfWidgetFormFilterInput(array('with_empty' => false)),
-      'updater_id' => new sfWidgetFormFilterInput(array('with_empty' => false)),
-      'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
-      'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
-      'deleted_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
+      'user_id'         => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'date'            => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'status'          => new sfWidgetFormChoice(array('choices' => array('' => '', 'Draft' => 'Draft', 'Pending' => 'Pending', 'Accepted' => 'Accepted', 'Denied' => 'Denied'))),
+      'leave_id'        => new sfWidgetFormFilterInput(),
+      'office_Entrance' => new sfWidgetFormFilterInput(),
+      'office_Exit'     => new sfWidgetFormFilterInput(),
+      'multiplier'      => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'creater_id'      => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'updater_id'      => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'created_at'      => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at'      => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'deleted_at'      => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
     ));
 
     $this->setValidators(array(
-      'user_id'    => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
-      'leave_id'   => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
-      'date'       => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDateTime(array('required' => false)))),
-      'status'     => new sfValidatorChoice(array('required' => false, 'choices' => array('Draft' => 'Draft', 'Pending' => 'Pending', 'Accepted' => 'Accepted', 'Denied' => 'Denied'))),
-      'multiplier' => new sfValidatorSchemaFilter('text', new sfValidatorNumber(array('required' => false))),
-      'creater_id' => new sfValidatorPass(array('required' => false)),
-      'updater_id' => new sfValidatorPass(array('required' => false)),
-      'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
-      'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
-      'deleted_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'user_id'         => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'date'            => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDateTime(array('required' => false)))),
+      'status'          => new sfValidatorChoice(array('required' => false, 'choices' => array('Draft' => 'Draft', 'Pending' => 'Pending', 'Accepted' => 'Accepted', 'Denied' => 'Denied'))),
+      'leave_id'        => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'office_Entrance' => new sfValidatorPass(array('required' => false)),
+      'office_Exit'     => new sfValidatorPass(array('required' => false)),
+      'multiplier'      => new sfValidatorSchemaFilter('text', new sfValidatorNumber(array('required' => false))),
+      'creater_id'      => new sfValidatorPass(array('required' => false)),
+      'updater_id'      => new sfValidatorPass(array('required' => false)),
+      'created_at'      => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at'      => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'deleted_at'      => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
     ));
 
     $this->widgetSchema->setNameFormat('working_hour_day_version_filters[%s]');
@@ -55,18 +59,20 @@ abstract class BaseWorkingHourDayVersionFormFilter extends BaseFormFilterDoctrin
   public function getFields()
   {
     return array(
-      'id'         => 'Number',
-      'user_id'    => 'Number',
-      'leave_id'   => 'Number',
-      'date'       => 'Date',
-      'status'     => 'Enum',
-      'multiplier' => 'Number',
-      'creater_id' => 'Text',
-      'updater_id' => 'Text',
-      'created_at' => 'Date',
-      'updated_at' => 'Date',
-      'deleted_at' => 'Date',
-      'version'    => 'Number',
+      'id'              => 'Number',
+      'user_id'         => 'Number',
+      'date'            => 'Date',
+      'status'          => 'Enum',
+      'leave_id'        => 'Number',
+      'office_Entrance' => 'Text',
+      'office_Exit'     => 'Text',
+      'multiplier'      => 'Number',
+      'creater_id'      => 'Text',
+      'updater_id'      => 'Text',
+      'created_at'      => 'Date',
+      'updated_at'      => 'Date',
+      'deleted_at'      => 'Date',
+      'version'         => 'Number',
     );
   }
 }
