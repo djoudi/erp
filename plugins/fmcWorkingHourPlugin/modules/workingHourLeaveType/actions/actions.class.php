b5e6f0dcac96b47a48c52426d26cb769fe883874
Yasin Aydin <yasin@yasinaydin.net>
2012-11-27 16:03:59 +0200

    Added leaveType module

---
+++ plugins/fmcWorkingHourPlugin/modules/workingHourLeaveType/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/BaseworkingHourLeaveTypeActions.class.php';
+
+/**
+ * workingHourLeaveType actions.
+ * 
+ * @package    fmcWorkingHourPlugin
+ * @subpackage workingHourLeaveType
+ * @author     Yasin Aydin (yasin@yasinaydin.net)
+ * @version    SVN: $Id: actions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+class workingHourLeaveTypeActions extends BaseworkingHourLeaveTypeActions
+{
+}
---
+++ plugins/fmcWorkingHourPlugin/modules/workingHourLeaveType/lib/BaseworkingHourLeaveTypeActions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base actions for the fmcWorkingHourPlugin workingHourLeaveType module.
+ * 
+ * @package     fmcWorkingHourPlugin
+ * @subpackage  workingHourLeaveType
+ * @author      Yasin Aydin (yasin@yasinaydin.net)
+ * @version     SVN: $Id: BaseActions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+abstract class BaseworkingHourLeaveTypeActions extends sfActions
+{
+}
