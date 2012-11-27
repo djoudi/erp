b0f863b711c0f1f2873524bc72225075e31b4217
Yasin Aydin <yasin@yasinaydin.net>
2012-11-27 14:11:44 +0200

    Created holiday plugin

---
+++ plugins/fmcHolidayPlugin/config/fmcHolidayPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * fmcHolidayPlugin configuration.
+ * 
+ * @package     fmcHolidayPlugin
+ * @subpackage  config
+ * @author      Yasin Aydin (yasin@yasinaydin.net)
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class fmcHolidayPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
