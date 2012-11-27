dc7781f781e41e6cf202c92ccb24aee00265887c
Yasin Aydin <yasin@yasinaydin.net>
2012-11-27 22:25:45 +0200

    Finished workingHourLeaveLimit

--- plugins/fmcWorkingHourPlugin/modules/workingHourLeaveLimit/actions/actions.class.php
+++ plugins/fmcWorkingHourPlugin/modules/workingHourLeaveLimit/actions/actions.class.php
@@ -3,4 +3,52 @@
 class workingHourLeaveLimitActions extends sfActions
 {
     
+    public function executeList (sfWebRequest $request)
+    {
+        $this->employees = Doctrine::getTable ('sfGuardUser')->findAll();
+        
+        $this->leaveTypes = Doctrine::getTable ('LeaveType')->findAll();
+    }
+    
+    public function executeEdit (sfWebRequest $request)
+    {
+        $this->employee = Doctrine::getTable('sfGuardUser')->findOneById($request->getParameter('id'));
+        
+        $this->forward404Unless ($this->employee);
+        
+        $this->limits = $this->employee->getLeaveRequestLimit()->toArray();
+        
+        $this->leaveTypes = Doctrine::getTable('LeaveType')->findAll()->toArray();
+        
+        $redirectUrl = $this->getController()->genUrl("@workingHourLeaveLimit_edit?id=".$this->employee->getId());
+        
+        if ($request->isMethod('post'))
+        {
+            foreach ($this->leaveTypes as $type)
+            {
+                $currentRecord = Doctrine::getTable ('LeaveRequestLimit')
+                    ->getForUserType($this->employee->getId(), $type['id']);
+                
+                if ($currentRecord['id'])
+                {
+                    $currentRecord->setLeaveLimit($request->getParameter($type['id']));
+                    $currentRecord->save();
+                }
+                elseif ($request->getParameter($type['id']))
+                {
+                    $newRecord = new LeaveRequestLimit();
+                    $newRecord->setUserId ($this->employee->getId());
+                    $newRecord->setTypeId ($type['id']);
+                    $newRecord->setLeaveLimit ($request->getParameter($type['id']));
+                    $newRecord->save();
+                }
+            }
+            
+            $this->getUser()->setFlash('success', 'Changes saved successfully.');
+            
+            $this->redirect($redirectUrl);
+            
+        }
+    }
+    
 }
---
+++ plugins/fmcWorkingHourPlugin/modules/workingHourLeaveLimit/templates/editSuccess.php
@@ -0,0 +1,42 @@
+<?php slot ('title', "Edit leave limits for: ".$employee) ?>
+
+<?php slot ('activeClass', "#topmenu_workinghours"); ?>
+
+<form class="form form-horizontal" method="post">
+
+    <table class="table table-condensed table-bordered table-hover">
+        <?php foreach ($leaveTypes as $type): ?>
+            <tr>
+                <th>
+                    <label class="control-label" for="<?php echo $type['id']; ?>"><?php echo $type['name']; ?></label>
+                </th>
+                <td>
+                    <?php
+                        $value=0;
+                        foreach ($limits as $limit)
+                        {
+                            if ($type['id'] == $limit['type_id'])
+                            {
+                                $value = $limit['leaveLimit'];
+                                break;
+                            }
+                        }
+                    ?>
+                    <?php if ($value): ?>
+                        <input type="text" name="<?php echo $type['id']; ?>" id="<?php echo $type['id']; ?>" value="<?php echo $value; ?>">
+                    <?php else: ?>
+                        <input type="text" name="<?php echo $type['id']; ?>" id="<?php echo $type['id']; ?>" value="">
+                        Default : <?php echo $type['default_Limit']; ?>
+                    <?php endif; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+    <div class="form-actions">
+        <a class="btn" href="<?php echo url_for('@workingHourLeaveLimit_list'); ?>">Back to List</a>
+        <input class="btn btn-primary" type="submit" value="Save" />
+    </div>
+
+</form>
+
