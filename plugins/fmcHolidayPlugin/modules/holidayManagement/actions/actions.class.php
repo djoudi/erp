e5bb39c16754436f093d2e9195243f70ffb9948d
Yasin Aydin <yasin@yasinaydin.net>
2012-11-27 14:26:58 +0200

    Added holiday management module

---
+++ plugins/fmcHolidayPlugin/modules/holidayManagement/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/BaseholidayManagementActions.class.php';
+
+/**
+ * holidayManagement actions.
+ * 
+ * @package    fmcHolidayPlugin
+ * @subpackage holidayManagement
+ * @author     Yasin Aydin (yasin@yasinaydin.net)
+ * @version    SVN: $Id: actions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+class holidayManagementActions extends BaseholidayManagementActions
+{
+}
