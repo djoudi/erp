f5f7b492a03769d25c0fca554492e2b9c66a4a78
Yasin Aydin <yasin@yasinaydin.net>
2012-12-07 02:15:07 +0200

    Refac: search deleted

--- plugins/fmcCustomerPlugin/modules/customerManagement/templates/listSuccess.php
+++ plugins/fmcCustomerPlugin/modules/customerManagement/templates/listSuccess.php
@@ -17,6 +17,21 @@
 <?php endif; ?>
 
 
-<div>
-    <?php include_partial ('items', array('items'=>$customers)); ?>
-</div>
+<table class="tablesorter tablesorterpager table table-hover table-bordered table-condensed">
+    <thead>
+        <tr>
+            <th>Name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($customers as $item): ?>
+            <tr>
+                <td>
+                    <a href="<?php echo url_for('@customerManagement_edit?id='.$item["id"]); ?>">
+                        <?php echo $item->getName(); ?>
+                    </a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
