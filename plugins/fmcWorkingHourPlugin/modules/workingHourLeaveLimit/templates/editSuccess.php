e92dc85000b70bfc2fe1e24e17b0ee7fe75c4fa8
Yasin Aydin <yasin@yasinaydin.net>
2012-12-26 20:48:33 +0200

    Fixed #65

--- plugins/fmcWorkingHourPlugin/modules/workingHourLeaveLimit/templates/editSuccess.php
+++ plugins/fmcWorkingHourPlugin/modules/workingHourLeaveLimit/templates/editSuccess.php
@@ -1,36 +1,56 @@
 <?php slot ('title', "Edit leave limits for: ".$employee) ?>
 
+
 <?php slot ('activeClass', "#topmenu_workinghours"); ?>
+
 
 <form method="post">
 
     <table class="table table-condensed table-bordered table-hover">
-        <?php foreach ($leaveTypes as $type): ?>
+        
+        <thead>
             <tr>
-                <th>
-                    <label class="control-label" for="<?php echo $type['id']; ?>"><?php echo $type['name']; ?></label>
-                </th>
-                <td>
-                    <?php
-                        $value=0;
-                        foreach ($limits as $limit)
-                        {
-                            if ($type['id'] == $limit['type_id'])
-                            {
-                                $value = $limit['leaveLimit'];
-                                break;
-                            }
-                        }
-                    ?>
-                    <?php if ($value): ?>
-                        <input type="text" name="<?php echo $type['id']; ?>" id="<?php echo $type['id']; ?>" value="<?php echo $value; ?>">
-                    <?php else: ?>
-                        <input type="text" name="<?php echo $type['id']; ?>" id="<?php echo $type['id']; ?>" value="">
-                        Default : <?php echo $type['default_Limit']; ?>
-                    <?php endif; ?>
-                </td>
+                <th>Leave Type</th>
+                <th>Limit</th>
+                <th>Default</th>
+                <th>Used</th>
             </tr>
-        <?php endforeach; ?>
+        
+        </thead>
+        
+        <tbody>        
+            <?php foreach ($leaveTypes as $type): ?>
+                <tr>
+                    <td>
+                        <label class="control-label" for="<?php echo $type['id']; ?>"><?php echo $type['name']; ?></label>
+                    </td>
+                    <td>
+                        <?php
+                            $value="";
+                            foreach ($limits as $limit)
+                            {
+                                if ($type['id'] == $limit['type_id'])
+                                {
+                                    $value = $limit['leaveLimit'];
+                                    break;
+                                }
+                            }
+                        ?>
+                        <input type="text" name="<?php echo $type['id']; ?>" id="<?php echo $type['id']; ?>" value="<?php echo $value; ?>">
+                    </td>
+                    
+                    <td>
+                        <?php echo $type['default_Limit']; ?>
+                    </td>
+                    
+                    <td>
+                        <?php echo whLeaveUser::countUsedLimit ($type['id'], $employee['id']); ?>
+                    </td>
+                    
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+        
     </table>
 
     <div class="form-actions">
