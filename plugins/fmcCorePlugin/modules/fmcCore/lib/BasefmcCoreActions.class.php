01ee5501efb944f16e26ec846e4bf2e12d743adf
Yasin Aydin <yasin@yasinaydin.net>
2013-01-10 02:10:41 +0200

    Refac: removed lucene search leftover

--- plugins/fmcCorePlugin/modules/fmcCore/lib/BasefmcCoreActions.class.php
+++ plugins/fmcCorePlugin/modules/fmcCore/lib/BasefmcCoreActions.class.php
@@ -2,25 +2,5 @@
 
 abstract class BasefmcCoreActions extends sfActions
 {
-    public function executeSearchupdate (sfWebRequest $request)
-    {
-        if ($tableName = $request->getParameter('tablename'))
-        {
-            $items = Doctrine::getTable('Customer')->findAll();
-            foreach ($items as $item)
-            {
-                $item->updateLuceneIndex();
-            }
-            $this->getUser()->setFlash('success', $tableName." search indexes built successfuly.");
-        }
-        else
-        {
-            $this->getUser()->setFlash('notice', 'Please specify a table name.');
-        }
-        
-        $url = $this->getController()->genUrl('@homepage');
-        $this->redirect ($url);
-        
-    }
     
 }
