b57b7016b0ba3a549f8b2ffdd606ea3129c0853c
Yasin Aydin <yasin@yasinaydin.net>
2013-01-10 11:50:35 +0200

    Refac: spaces

--- plugins/fmcCorePlugin/lib/FmcFilter.class.php
+++ plugins/fmcCorePlugin/lib/FmcFilter.class.php
@@ -1,53 +1,58 @@
 <?php
     
-class FmcFilter {
+class FmcFilter
+{
     
     private $formName;
     private $user;
     private $filter;
     private $filtered;
     private $controller;
-
-    public function __construct($formname) {
-  
+    
+    
+    public function __construct($formname)
+    {
         $this->formName = $formname;
         $this->user = sfContext::getInstance()->getUser();
-        $this->controller = sfContext::getInstance()->getController();
-        
+        $this->controller = sfContext::getInstance()->getController();   
     }
     
-    public function getFilter() {
     
+    public function getFilter()
+    {
         return ($this->filter);
     }
     
-    public function getFiltered() {
-  
+    
+    public function getFiltered()
+    {
         return ($this->filtered);
-        
     }
     
-    public function resetForm() {
     
-        $this->setFilters (array());
-        $this->controller->redirect($request->getReferer());
-        
+    public function resetForm()
+    {
+        $this->setFilters(array());
+        $this->controller->redirect($request->getReferer());   
     }
     
-    public function initFilterForm (sfWebRequest $request, Doctrine_Query $q) {
     
+    public function initFilterForm (sfWebRequest $request, Doctrine_Query $q)
+    {
         $this->filter = new $this->formName ($this->getFilters());
   
-        if($request->isMethod('post')) {
-    
-            if($request->hasParameter('_reset')) {
+        if($request->isMethod('post'))
+        {
+            if($request->hasParameter('_reset'))
+            {
                 $this->setFilters (array());
                 $this->controller->redirect($request->getReferer());  
             }
     
             $this->filter->bind ( $request->getParameter ($this->filter->getName()) );
     
-            if($this->filter->isValid()) {
+            if($this->filter->isValid())
+            {
                 $this->setFilters($this->filter->getValues());
                 $this->controller->redirect($request->getReferer());
             }
@@ -59,12 +64,15 @@ class FmcFilter {
         return $this->filter->buildQuery($this->getFilters());
     }
     
-    public function getFilters() {
-        
+    
+    public function getFilters()
+    {    
         return $this->user->getAttribute('cf_filters', array());
     }
     
-    public function setFilters($filters) {
+    
+    public function setFilters($filters)
+    {
   
         $this->user->setAttribute('cf_filters', $filters);
     }
