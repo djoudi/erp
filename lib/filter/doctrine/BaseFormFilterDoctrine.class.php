605cc9e1e80f33d780486974361e5ec0719f00db
Yasin Aydin <yasin@yasinaydin.net>
2013-01-22 12:41:49 +0200

    Refac: Spaces

--- lib/filter/doctrine/BaseFormFilterDoctrine.class.php
+++ lib/filter/doctrine/BaseFormFilterDoctrine.class.php
@@ -1,22 +1,14 @@
 <?php
 
-/**
- * Project filter form base class.
- *
- * @package    cfdb
- * @subpackage filter
- * @author     Your name here
- * @version    SVN: $Id: sfDoctrineFormFilterBaseTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
- */
 abstract class BaseFormFilterDoctrine extends sfFormFilterDoctrine
 {
-  public function setup()
-  {
-    unset($this['created_at']);
-    unset($this['updated_at']);
-    unset($this['deleted_at']);
-    unset($this['version']);
-    unset($this['creater_id']);
-    unset($this['updater_id']);
-  }
+    public function setup()
+    {
+        unset($this['created_at']);
+        unset($this['updated_at']);
+        unset($this['deleted_at']);
+        unset($this['version']);
+        unset($this['creater_id']);
+        unset($this['updater_id']);
+    }
 }
--- lib/model/doctrine/MyDoctrineRecord.class.php
+++ lib/model/doctrine/MyDoctrineRecord.class.php
@@ -2,32 +2,32 @@
  
 abstract class MyDoctrineRecord extends sfDoctrineRecord
 {
-  protected $systemUserId = null;
- 
-  public function preSave($event)
-  {
-    if (!$this->getSystemUser())
+    protected $systemUserId = null;
+    
+    public function preSave($event)
     {
-      if(!sfContext::hasInstance() || get_called_class()=='sfGuardUser')
-      {
-        $userId = sfConfig::get('default_updater_id');
-        $this->setSystemUser($userId);
-      }
-      else
-      {
-        $this->setSystemUser(sfContext::getInstance()->getUser()->getGuardUser()->getId());
-      }
+        if (!$this->getSystemUser())
+        {
+            if(!sfContext::hasInstance() || get_called_class()=='sfGuardUser')
+            {
+                $userId = sfConfig::get('default_updater_id');
+                $this->setSystemUser($userId);
+            }
+            else
+            {
+                $this->setSystemUser(sfContext::getInstance()->getUser()->getGuardUser()->getId());
+            }
+        }
     }
-  }
- 
-  public function setSystemUser($userId)
-  {
-    $this->systemUserId = $userId;
-  }
- 
-  public function getSystemUser()
-  {
-    return $this->systemUserId;
-  }
+    
+    public function setSystemUser($userId)
+    {
+        $this->systemUserId = $userId;
+    }
+    
+    public function getSystemUser()
+    {
+        return $this->systemUserId;
+    }
 }
 ?>
