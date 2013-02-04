c93f54f67f60a64a338bd8d778ac22332652d914
Yasin Aydin <yasin@yasinaydin.net>
2013-02-04 18:11:02 +0200

    Refac: spaces

--- plugins/fmcEmployeePlugin/modules/departmentManagement/templates/editSuccess.php
+++ plugins/fmcEmployeePlugin/modules/departmentManagement/templates/editSuccess.php
@@ -15,11 +15,9 @@
     </tbody>
 </table>
 
-
 <?php include_partial('fmcCore/recordForm', array(
     "form" => $form, 
     "title" => "Department: ".$item, 
     "activeClass" => "#topmenu_settings", 
     "back_url" => url_for("@departmentManagement_list")
 )); ?>
-
