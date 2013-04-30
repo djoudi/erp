c7da8f13ef949a7af15959c7e74ee433597e4f4e
Yasin Aydin <yasin@yasinaydin.net>
2013-04-30 15:24:31 +0400

    Some testing values

--- plugins/fmcWorkingHourPlugin/modules/wh_user_reports/actions/actions.class.php
+++ plugins/fmcWorkingHourPlugin/modules/wh_user_reports/actions/actions.class.php
@@ -5,17 +5,13 @@ class wh_user_reportsActions extends sfActions
     public function executeBydate (sfWebRequest $request)
     {
         $user_id = $this->getUser()->getGuardUser()->getId();
-        $user_id = 39;
+        $user_id = 39; // For testing
         
-        $this->startDate = "2013-01-01";
+        $this->startDate = "2013-01-01"; // This should be changed to the month or week
         $this->endDate = date("Y-m-d");
         
         $class = new whReport();
-        
         $this->results = $class->BalanceResultsForDateInterval ($user_id, $this->startDate, $this->endDate);
-        
         $this->lastBalance = $class->getUpToDayBalance();
-        
-        #$this->lastBalance = $class->calculateEmployeeBalanceToDate ($user_id, $this->startDate);
     }
 }
